3ad6740550d2e02ab5f6343087986d17ff62f11f
saleborg <Stefan.aleborg@gmail.com>
2021-11-04 20:34:02 +0100

    Added frontend project

---
+++ energy-price-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import EnergyPriceList from './components/EnergyPriceList';
+
+function App() {
+  return (
+    <div className="App">
+      <EnergyPriceList />
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ energy-price-frontend/src/components/EnergyPriceList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const EnergyPriceList =() => {
+    return(<p>Test</p>)
+}
+
+export default EnergyPriceList
